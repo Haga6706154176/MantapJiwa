589136245610c1ff94475a244286ff888de0fc1e
M. TRI AL HAGA <al.haga96@gmail.com>
2017-04-06 09:41:41 +0700

    Add files via upload

---
+++ faktorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+     public static void main(String[] args)
+     {
+         System.out.print("Enter a number: ");
+         Scanner input = new Scanner(System.in);
+         int num = input.nextInt();
+         long factorialResult = factorialRecursive(num);
+         System.out.println(factorialResult);
+     }
+
+
+     public static long factorialRecursive(int n) {
+         if (n == 0 || n == 1 )
+             return 1;
+
+         return n * factorialRecursive(n - 1);
+     }
+}
